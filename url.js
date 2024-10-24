68fdf37ee9ac7d33ef5a5053144447a4edc8b8b6
linlin9805 <succeed211148@gmail.com>
2024-10-24 23:04:10 +0900

    Update url.js

--- url.js
+++ url.js
@@ -8,7 +8,6 @@
 
 
 const urls = [
-	'https://zh.surveymonkey.com/r/BXTGB27',
 	'https://zh.surveymonkey.com/r/B9VLWPT',
 	'https://zh.surveymonkey.com/r/BXG9P8R',
 	'https://zh.surveymonkey.com/r/BXRKMGH'
